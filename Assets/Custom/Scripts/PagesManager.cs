9b8dfdcf6038f973e05ac4e9bc31f74e0f54c088
Krost22 <eduardomogollonsalcedo@gmail.com>
2024-08-05 15:48:31 -0500

    Commit inicial
    Dos vistas hechas

---
+++ Assets/Custom/Scripts/PagesManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PagesManager : MonoBehaviour
+{
+    public GameObject pageToShow;
+    public GameObject pageToHide;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePage()
+    {
+        pageToShow.SetActive(true);
+        pageToHide.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
